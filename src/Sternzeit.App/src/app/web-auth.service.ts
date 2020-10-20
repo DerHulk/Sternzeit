cbcbd8f294abcba77823d6cbb15d77ef48acdbfc
thegrayhulk <thegrayhulk@googlemail.com>
2020-10-20 22:07:25 +0200

    Start implementation of web-Auth-Service.

--- src/Sternzeit.App/src/app/web-auth.service.ts
+++ src/Sternzeit.App/src/app/web-auth.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { stringify } from 'querystring';
+import { ThrowStmt } from '@angular/compiler';
+import { environment } from 'src/environments/environment';
+import { promise } from 'protractor';
+import { Observable, pipe } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 interface WebAuthWindow {
   PublicKeyCredential?: any;
+}
 
+class RegisterPreconditions {
+  Challenge: string;
+  RelayingPartyId: string;
+  RelayingPartyName: string;
+
+  UserName: string;
+  UserDisplayName: string;
+  UserId: string;
+
+  RegisterUrl: string;
 }
 
 declare var window: WebAuthWindow;
@@ -12,55 +30,93 @@ declare var window: WebAuthWindow;
 })
 export class WebAuthService {
 
-  constructor() {
-
+  constructor(private http: HttpClient) {
   }
 
   public isAvailable(): Boolean {
     return window.PublicKeyCredential ? true : false;
   }
 
-  public register(challenge:string): void {
+  public loadPreconditions(userName: string): Observable<RegisterPreconditions> {
+
+    return this.http.get<RegisterPreconditions>(environment.registrationEndPoint)
+      .pipe(map(x => { x.UserName = userName; return x; }));
+  }
+
+  public register(preconditions: RegisterPreconditions): void {
 
     let keyType: PublicKeyCredentialType;
+    const challengeBuffer = this.ConvertToBuffer(preconditions.Challenge);
+
     navigator.credentials.create({
       publicKey: {
-        challenge: null,
-        // Request ES256 algorithm ???
+        // Request ES256 algorithm
+        //https://developer.mozilla.org/en-US/docs/Web/API/PublicKeyCredentialCreationOptions/pubKeyCredParams
         pubKeyCredParams: [{ alg: -7, type: keyType }],
-        rp: { id: 'relaying-party', name: '' },
-        user: { name: '', displayName: '', id: new TextEncoder().encode("xxxx") }
+        challenge: challengeBuffer,
+        rp: {
+          id: preconditions.RelayingPartyId,
+          name: preconditions.RelayingPartyName
+        },
+        user: {
+          id: new TextEncoder().encode(preconditions.UserId),
+          name: preconditions.UserName,
+          displayName: preconditions.UserName,
+        }
       }
     }).then((credential) => {
+      const credentialsAsJson = this.ConvertToJson(credential);
 
-      var publicKeyCredentialToJSON = (pubKeyCred) => {
-        if (pubKeyCred instanceof Array) {
-          let arr = [];
-          for (let i of pubKeyCred)
-            arr.push(publicKeyCredentialToJSON(i));
+      this.http.post<any>(preconditions.RegisterUrl, credentialsAsJson, { observe: 'response' })
+        .subscribe(x => {
 
-          return arr;
-        }
-
-        if (pubKeyCred instanceof ArrayBuffer) {
-          return btoa(String.fromCharCode.apply(null, new Uint8Array(pubKeyCred)));
-        }
-
-        if (pubKeyCred instanceof Object) {
-          let obj = {};
-
-          for (let key in pubKeyCred) {
-            obj[key] = publicKeyCredentialToJSON(pubKeyCred[key]);
+          if (x.ok) {
+            //we expected a back url in response.
+          }
+          else {
+           throw new Error('That have not worked.');
           }
 
-          return obj;
-        }
+        });
 
-        return pubKeyCred;
+    });
+  }
+
+  private ConvertToBuffer(value: string) {
+    const byteCharacters = atob(value);
+    const byteNumbers = new Array(byteCharacters.length);
+
+    for (let i = 0; i < byteCharacters.length; i++) {
+      byteNumbers[i] = byteCharacters.charCodeAt(i);
+    }
+    return new Uint8Array(byteNumbers);
+  }
+
+  private ConvertToJson(credentials: Credential) {
+    if (credentials instanceof Array) {
+      const arr = [];
+      for (let i of credentials) {
+        arr.push(this.ConvertToJson(i));
       }
 
-      var credentialsAsJson = publicKeyCredentialToJSON(credential);
-
-
+      return arr;
     }
+
+    if (credentials instanceof ArrayBuffer) {
+      return btoa(String.fromCharCode.apply(null, new Uint8Array(credentials)));
+    }
+
+    if (credentials instanceof Object) {
+      const obj = {};
+
+      for (let key in credentials) {
+        obj[key] = this.ConvertToJson(credentials[key]);
+      }
+
+      return obj;
+    }
+
+    return credentials;
+  }
+
 }
--- src/Sternzeit.App/src/environments/environment.prod.ts
+++ src/Sternzeit.App/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  registrationEndPoint: 'http://localhost:12345'
 };
