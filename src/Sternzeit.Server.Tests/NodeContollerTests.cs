8a0febeeebb5a6a293e91d60cf46781a1bb9014b
thegrayhulk <thegrayhulk@googlemail.com>
2020-12-26 22:49:49 +0100

    Extende tests.

--- src/Sternzeit.Server.Tests/NodeContollerTests.cs
+++ src/Sternzeit.Server.Tests/NodeContollerTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
 using Moq;
 using Sternzeit.Server.Controllers;
 using Sternzeit.Server.Services;
@@ -6,6 +7,7 @@ using Sternzeit.Server.States;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -24,17 +26,34 @@ namespace Sternzeit.Server.Tests
             this.Target = new NodeController(this.TimeService.Object, this.MongoDb);
         }
 
-        [Fact(DisplayName ="Check that a valid note will be created.")]
+        [Fact(DisplayName ="Check that a valid note will be created with ok.")]
         public async Task Create01()
+        {
+            //arrange
+            var titel = "test";
+            var collection = this.MongoDb.CreateMockCollection<NoteStates>("Notes");
+            
+            //act
+            var result = await this.Target.Create(titel);
+
+            //assert
+            Assert.IsType<OkResult>(result);
+            collection.Verify(x => x.InsertOneAsync(It.Is<NoteStates>(x => x.Titel == titel), It.IsAny<InsertOneOptions>(), It.IsAny<CancellationToken>()));
+        }
+
+        [Theory(DisplayName = "Check that no content will be returned if the titel is null/empty.")]
+        [InlineData("")]
+        [InlineData(null)]
+        public async Task Create02(string titel)
         {
             //arrange
             this.MongoDb.CreateMockCollection<NoteStates>("Notes");
 
             //act
-            var result = await this.Target.Create("test");
+            var result = await this.Target.Create(titel);
 
             //assert
-            Assert.IsType<OkResult>(result);            
+            Assert.IsType<NoContentResult>(result);
         }
 
     }
